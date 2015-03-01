edb8a7aba23f8870b5a7bc90dd3bc60054a25842
Joshua Smith <joshsmith01@me.com>
2015-02-28 16:03:13 -0800

    - ADDED Slider - ADDED icon-fonts to the slider navigation - FINISHED top-bar navigation

--- front-page.php
+++ front-page.php
@@ -1,32 +1,21 @@
 <?php get_header(); ?>
 
-<div class="row">
-  <div class="small-12 large-8 columns" role="main">
-
+<?php layerslider(1) ?>
   <?php do_action('foundationPress_before_content'); ?>
 
-  <?php while (have_posts()) : the_post(); ?>
-    <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-      <header>
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-      </header>
-      <?php do_action('foundationPress_page_before_entry_content'); ?>
-      <div class="entry-content">
-        <?php the_content(); ?>
+
+    <div class="row top-services-banner">
+      <div class="small-9 columns">
+        <h2>Some of our top Services</h2>
+        <p>Ut eleifend libero sed neque rhoncus consequat. Maecenas tincidunt, augue et rutrum condimentum, libero lectus mattis orci, ut commodo.</p>
       </div>
-      <footer>
-        <?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
-        <p><?php the_tags(); ?></p>
-      </footer>
-      <?php do_action('foundationPress_page_before_comments'); ?>
-      <?php comments_template(); ?>
-      <?php do_action('foundationPress_page_after_comments'); ?>
-    </article>
-  <?php endwhile;?>
-
+      <div class="small-3 columns">
+        <div class="button small-centered radius">View More</div>
+      </div>
+    </div>
+    
+    
+    
+    
   <?php do_action('foundationPress_after_content'); ?>
-
-  </div>
-  <?php get_sidebar(); ?>
-</div>
 <?php get_footer(); ?>
