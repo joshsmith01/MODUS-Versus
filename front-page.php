2f167dbf8392a0f33a87597110a4b0da82c27341
Joshua Smith <joshsmith01@me.com>
2015-03-20 09:34:06 -0700

    ADDED stats section html

--- front-page.php
+++ front-page.php
@@ -18,30 +18,71 @@
     
     
     
-    <div class="too-legit-container">
-  
-      <section class="row">
-        <div class="legit-title small-12 columns">
-          <h4>Our Happy Clients</h4>
+    <section class="stats-container">
+      <div class="row stats">
+        <div class="small-3 columns choose-us">
+          <h3>Why Choose Us?</h3>
+          <ul class="list-group">
+            <li class="list-group-item"><i class="fa fa-arrow-right fa-fw"></i>Lorem ipsum dolor sit.</li>
+            <li class="list-group-item"><i class="fa fa-arrow-right fa-fw"></i>Temporibus mollitia fugiat repudiandae?</li>
+            <li class="list-group-item"><i class="fa fa-arrow-right fa-fw"></i>Veniam officia voluptatibus dolorum.</li>
+            <li class="list-group-item"><i class="fa fa-arrow-right fa-fw"></i>Beatae, dolorem, perferendis. Sunt.</li>
+            <li class="list-group-item"><i class="fa fa-arrow-right fa-fw"></i>Tenetur unde, at aperiam!</li>
+            <li class="list-group-item"><i class="fa fa-arrow-right fa-fw"></i>Assumenda qui molestias ab.</li>
+          </ul>
         </div>
-          <div class="legit small-2 columns">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo_03.jpg" alt="jquery-logo_03" width="102" height="26" />
-           
-          </div>
-          <div class="legit small-2 columns">
-           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo_03.jpg" alt="jquery-logo_03" width="102" height="26" />          </div>
-          <div class="legit small-2 columns">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo_03.jpg" alt="jquery-logo_03" width="102" height="26" />
-          </div>
-          <div class="legit small-2 columns">
-           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo_03.jpg" alt="jquery-logo_03" width="102" height="26" />
-          </div>
-          <div class="legit small-2 columns">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo_03.jpg" alt="jquery-logo_03" width="102" height="26" />
-          </div>
-          <div class="legit small-2 columns">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo_03.jpg" alt="jquery-logo_03" width="102" height="26" />          </div>
-      </section>
-    </div>
+        <div class="small-6 columns circle-graphs">
+          <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, harum, dignissimos! Suscipit harum, corporis modi!
+            <div class="small-1"><img src="" alt="">
+                <h4>Lorem.</h4>
+              </img></div>
+            <div class="small-1"><img src="" alt="">
+                <h4>Ullam.</h4>
+              </img></div>
+            <div class="small-1"><img src="" alt="">
+                <h4>Minus!</h4>
+              </img></div>
+            <div class="small-1"><img src="" alt="">
+                <h4>Quas.</h4>
+              </img></div>
+          </h3>
+        </div>
+        <div class="small-3 columns single-testimonial">
+          <h3>What Clients Say?</h3>
+          <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi necessitatibus quam eius aut temporibus ipsa sed autem labore nihil eligendi.<cite>John Doe</cite></blockquote>
+        </div>
+      </div><!--  END stats -->
+    </section><!--  END stats-container -->
+    
+    
+    <div class="too-legit-container">
+      <div class="row too-legit">
+      <div class="legit-title">
+        <h4>Our Happy Clients</h4>
+      </div>
+        <ul class="small-block-grid-6">
+         <li>
+          <p class="legit"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo.png" alt="jquery-logo_03" width="102" height="26" /></p>
+          </li>
+          <li>
+          <p class="legit"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo.png" alt="jquery-logo_03" width="102" height="26" /></p>
+          </li>
+         <li>
+          <p class="legit"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo.png" alt="jquery-logo_03" width="102" height="26" /></p>
+          </li>
+         <li>
+          <p class="legit"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo.png" alt="jquery-logo_03" width="102" height="26" /></p>
+          </li>
+         <li>
+          <p class="legit"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo.png" alt="jquery-logo_03" width="102" height="26" /></p>
+          </li>
+         <li>
+          <p class="legit"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/jquery-logo.png" alt="jquery-logo_03" width="102" height="26" /></p>
+          </li>
+
+          
+        </ul>
+        </div>
+      </div><!--  END too-legit-container -->
   <?php do_action('foundationPress_after_content'); ?>
 <?php get_footer(); ?>
