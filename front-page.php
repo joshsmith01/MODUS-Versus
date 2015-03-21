40931ebcd8fcedbc7b2b0fad03817efc1655e7fa
Joshua Smith <joshsmith01@me.com>
2015-03-20 23:16:18 -0700

    Completed the services section

--- front-page.php
+++ front-page.php
@@ -4,23 +4,80 @@
 
 
 
+  <div class="top-services-container">
     <div class="row top-services-banner">
-    <div class="small-12 columns">
-    <h2>Some of our top Services</h2>
-    </div>
-      <div class="small-12 medium-6 large-9 columns">
-        <p>Ut eleifend libero sed neque rhoncus consequat. Maecenas tincidunt, augue et rutrum condimentum, libero lectus mattis orci, ut commodo.</p>
+      <div class="small-12 columns">
+      <h2>Some of our top Services</h2>
       </div>
-      <div class="small-12 medium-6 large-3 columns button-holder">
-        <div class="button small-centered radius">View More</div>
+        <div class="small-12 medium-6 large-9 columns">
+          <p>Ut eleifend libero sed neque rhoncus consequat. Maecenas tincidunt, augue et rutrum condimentum, libero lectus mattis orci, ut commodo.</p>
+        </div>
+        <div class="small-12 medium-6 large-3 columns button-holder">
+        <div class="button large small-centered radius">View More</div>
       </div>
     </div>
+  </div>
+   
+    <div class="services-container"> <!--  START services -->
+      <div class="row services" data-equalizer>
+        <div class="small-12 medium-6 large-3 columns service" data-equalizer-watch>
+          <div class="service-bg">
+            <i class="fa fa-thumbs-o-up"></i>
+            <h3>SUSPENDISSE</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde alias impedit velit sit ea!</p>
+            <div class="button">Read More</div>
+          </div>
+        </div>
+        <div class="small-12 medium-6 large-3 columns service" data-equalizer-watch>
+          <div class="service-bg">
+            <i class="fa fa-key"></i>
+            <h3>MAECENAS</h3><p>Iste architecto omnis, esse facere quod ratione, officia veritatis fugit.</p>
+            <div class="button">Read More</div>
+          </div>
+        </div>
+        <div class="small-12 medium-6 large-3 columns service" data-equalizer-watch>
+          <div class="service-bg">
+            <i class="fa fa-flag"></i>
+            <h3>ALIQUAM</h3><p>Iste architecto omnis, esse facere quod ratione, officia veritatis fugit.</p>
+            <div class="button">Read More</div>
+          </div>
+        </div>
+        <div class="small-12 medium-6 large-3 columns service" data-equalizer-watch>
+          <div class="service-bg">
+            <i class="fa fa-flask"></i>
+            <h3>HABITASSE</h3><p>Ducimus voluptates assumenda, illum adipisci sed quaerat est ipsum nisi.</p>
+            <div class="button">Read More</div>
+          </div>
+        </div>
+      </div><!--  END services -->
+    </div><!--  END services-container -->
+    <!--  START carousel -->
+    
+    <div class="carousel-container">
+      <div class="row carousel">
+        <h2 class="small-12 columns text-center">Why Modus Versus?</h2>
+        <p class="small-12 columns text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, voluptate.</p>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+    
+    
+    
+    <!--  END carousel -->
+    
+    
+    
+    
+    
     
     
     
     <section class="stats-container">
       <div class="row stats">
-        <div class="small-3 columns choose-us">
+        <div class="small-12 medium-3 columns choose-us">
           <h3>Why Choose Us?</h3>
           <ul class="list-group">
             <li class="list-group-item"><i class="fa fa-arrow-right fa-fw"></i>Lorem ipsum dolor sit.</li>
@@ -31,23 +88,27 @@
             <li class="list-group-item"><i class="fa fa-arrow-right fa-fw"></i>Assumenda qui molestias ab.</li>
           </ul>
         </div>
-        <div class="small-6 columns circle-graphs">
+        <div class="small-12 medium-6 columns circle-graphs">
           <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, harum, dignissimos! Suscipit harum, corporis modi!
-            <div class="small-1"><img src="" alt="">
-                <h4>Lorem.</h4>
-              </img></div>
-            <div class="small-1"><img src="" alt="">
-                <h4>Ullam.</h4>
-              </img></div>
-            <div class="small-1"><img src="" alt="">
-                <h4>Minus!</h4>
-              </img></div>
-            <div class="small-1"><img src="" alt="">
-                <h4>Quas.</h4>
-              </img></div>
+            <div class="row">
+              <div class="small-3 columns">
+              <div id="circle">
+              </div>
+                  <h4>Lorem</h4>
+                </img></div>
+              <div class="small-3 columns"><img src="" alt="">
+                  <h4>Ullam</h4>
+                </img></div>
+              <div class="small-3 columns"><img src="" alt="">
+                  <h4>Minus</h4>
+                </img></div>
+              <div class="small-3 columns"><img src="" alt="">
+                  <h4>Quas</h4>
+                </img></div>
+            </div>
           </h3>
         </div>
-        <div class="small-3 columns single-testimonial">
+        <div class="small-12 medium-3 columns single-testimonial">
           <h3>What Clients Say?</h3>
           <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi necessitatibus quam eius aut temporibus ipsa sed autem labore nihil eligendi.<cite>John Doe</cite></blockquote>
         </div>
